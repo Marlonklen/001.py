de9c0c95055723eaae27d14937a8d2b94ed4f4fb
Marlonklen <107556312+Marlonklen@users.noreply.github.com>
2023-01-08 16:43:34 +0000

    Primeiro desafio em programação Python.

---
+++ ex086.py
@@ -0,0 +1,10 @@
+""" Crie um programa que declare uma matriz de dimensão 3×3 e
+preencha com valores lidos pelo teclado. No final,
+ mostre a matriz na tela, com a formatação correta."""
+
+linha1 = list([],[],[])
+linha2 = list([],[],[])
+linha3 = list([],[],[])
+
+for c in range():
+    valores = int(input("Digite o valor: "))

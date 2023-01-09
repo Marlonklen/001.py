e4e4af11f31e32257e66ba822d03c089ecf7a066
Marlonklen <107556312+Marlonklen@users.noreply.github.com>
2023-01-09 10:54:27 +0000

    desafio 86 resolvido.

--- ex086.py
+++ ex086.py
@@ -1,10 +1,14 @@
 """ Crie um programa que declare uma matriz de dimensão 3×3 e
 preencha com valores lidos pelo teclado. No final,
  mostre a matriz na tela, com a formatação correta."""
+matriz = ([0,0,0],[0,0,0],[0,0,0])
 
-linha1 = list([],[],[])
-linha2 = list([],[],[])
-linha3 = list([],[],[])
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f"Digite o valor para posição [{l}, {c}] : "))
+print("=-"* 30)
+for l in range(0,3):
+    for c in range(0,3):
+        print(f"[{matriz[l][c]:^5}]", end=" ")
+    print()
 
-for c in range():
-    valores = int(input("Digite o valor: "))

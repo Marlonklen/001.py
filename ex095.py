20ab4c019ded9fadd15c05bf577c80e2fc97bd60
Marlonklen <107556312+Marlonklen@users.noreply.github.com>
2023-03-22 11:27:07 +0000

    aproveitamento de jogadores biblioteca mais completa.

--- ex095.py
+++ ex095.py
@@ -1,25 +1,50 @@
-"""Crie um programa que gerencie o aproveitamento de um jogador de futebol.
-O programa vai ler o nome do jogador e quantas partidas ele jogou.
-Depois vai ler a quantidade de gols feitos em cada partida. No final,tudo isso
-será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato."""
+"""Aprimore o desafio 93 para que ele funcione com vários jogadores,
+incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador."""
 
+time = list()
 jogador = dict()
 partidas = list()
-jogador['nome'] = str(input('Nome do jogador: '))
-tot = int(input(f'Quantas partidas {jogador["nome"]} jogou: '))
-for c in range(0, tot):
-    partidas.append( int(input(f'   Quantos gols na {c+1}ª partida: ')))
-jogador['gols'] = partidas[:]
-jogador['total'] = sum(partidas)
-print('-='* 30)
-print(jogador)
-print('-='* 30)
-for k, v in jogador.items():
-    print(f'O campo {k} tem o valor {v}')
-print('-=' * 30)
-print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
-for i, v in enumerate(jogador['gols']):
-    print(f'     => Na partida {i+1}, fez {v} gols.')
-print(f'Foi um total de {jogador["total"]} gols.')
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou: '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append( int(input(f'   Quantos gols na {c+1}ª partida: ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Digite apenas S ou N. ')
+    if resp == 'N':
+        break
+print('-=' * 40)
+print('COD ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-='* 40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-='*40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar.) '))
+    if busca == 999:
+        break
+    if busca > len(time):
+        print(f'ERRO! Não existe jogador com o COD {busca}!')
+    else:
+        print(f'----LEVANTAMENTO DO JOAGADOR {time[busca]["nome"]}:')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'     No jogo {i+1} fez {g} gols.')
+    print('-='*40)
+print('<<<< VOLTE SEMPRE >>>>')
+
 
 
